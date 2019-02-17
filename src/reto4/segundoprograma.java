5d8e4ec9d20117b9cd641f7cc4582861f97bef86
Ulises101199 <Ulises-101199@hotmail.com>
2019-02-16 18:51:11 -0600

    reto4

---
+++ src/reto4/segundoprograma.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reto4;
+
+/**
+ *
+ * @author Ulises
+ */
+public class segundoprograma {
+    
+    public String nombre;
+    public int edad;
+    public double altura;
+    
+    
+    public void saludar (){
+    }
+    
+    public void despedirse(){
+    }
+    
+    public void retirarse (){
+    }
+    
+    
+}
